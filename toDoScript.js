6b730c136eff542e29e4a9bd2f2e3beb92a11d5a
cabarros3 <camillaerbarros@gmail.com>
2021-07-06 05:01:21 -0300

    Cria exercício to do list

---
+++ toDoScript.js
@@ -0,0 +1,7 @@
+const listContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector("[data-new-list-form]");
+const newListInput = document.querySelector("[data-new-list-input]");
+
+newListForm.addEventListener("submit", function (e) {});
+
+// dica: toda vez que um submit é recebido a página é atualizada
